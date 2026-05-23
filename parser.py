249acbc9c1eabe4f6e4e9077a8cb05188d91da1c
wilsoonnaa <114656832+wilsoonnaa@users.noreply.github.com>
2026-05-23 19:58:51 +0300

    Implement parse_model function

--- parser.py
+++ parser.py
@@ -1,6 +1,50 @@
+import xml.etree.ElementTree as ET
+
 def parse_model(xml_path: str) -> dict:
-    raise NotImplementedError
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
 
+    class_elements = root.findall('Class')
+    
+    classes = {}
+    aggregations = []
+    root_class = None
 
+    for class_element in class_elements:
+        name = class_element.get('name')
+        documentation = class_element.get("documentation", "")
+        is_root = class_element.get("isRoot") == "true"
+
+        attributes = []
+        for attribute_element in class_element.findall('Attribute'):
+            attributes.append({
+                "name": attribute_element.get("name"),
+                "type": attribute_element.get("type"),
+            })
+
+        classes[name] = {
+            "documentation": documentation,
+            "isRoot": is_root,
+            "attributes": attributes,
+        }
+
+        if is_root:
+            root_class = name
+
+    for aggregation_element in root.findall("Aggregation"):
+        aggregations.append({
+            "source": aggregation_element.get("source"),
+            "target": aggregation_element.get("target"),
+            "sourceMultiplicity": aggregation_element.get("sourceMultiplicity"),
+            "targetMultiplicity": aggregation_element.get("targetMultiplicity"),
+        })
+
+    return {
+        "classes": classes,
+        "aggregations": aggregations,
+        "root_class": root_class,
+    }
+
+                
 def get_children_by_parent(model: dict) -> dict:
     raise NotImplementedError
